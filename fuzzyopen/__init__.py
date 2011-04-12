9ebddf87d35cf36bc091242dbe867ccc8472c99b
Andrew Liu <andrewliu33@gmail.com>
2011-04-12 11:23:17 +0800

    Initialization for gedit3

--- fuzzyopen/__init__.py
+++ fuzzyopen/__init__.py
@@ -1,19 +1,22 @@
-import gedit
+from gi.repository import GObject, Gedit
 from fuzzyopen import FuzzyOpenPluginInstance
 from config import FuzzyOpenConfigWindow
 
 # STANDARD PLUMMING
-class FuzzyOpenPlugin( gedit.Plugin ):
+class FuzzyOpenPlugin( GObject.Object, Gedit.WindowActivatable ):
+  __gtype_name__ = "FuzzyOpenPlugin"
+
   DATA_TAG = "FuzzyOpenPluginInstance"
+  window = GObject.property(type=Gedit.Window)
 
   def __init__( self ):
-    gedit.Plugin.__init__( self )
+    GObject.Object.__init__( self )
 
-  def _get_instance( self, window ):
-    return window.get_data( self.DATA_TAG )
+  def _get_instance( self ):
+    return self.window.get_data( self.DATA_TAG )
 
-  def _set_instance( self, window, instance ):
-    window.set_data( self.DATA_TAG, instance )
+  def _set_instance( self, instance ):
+    self.window.set_data( self.DATA_TAG, instance )
 
   def is_configurable( self ):
     return True
@@ -21,13 +24,13 @@ class FuzzyOpenPlugin( gedit.Plugin ):
   def create_configure_dialog( self ):
     return FuzzyOpenConfigWindow()._window
 
-  def activate( self, window ):
-    self._set_instance( window, FuzzyOpenPluginInstance( self, window ) )
+  def activate( self ):
+    self._set_instance( FuzzyOpenPluginInstance( self ) )
 
-  def deactivate( self, window ):
-    self._get_instance( window ).deactivate()
-    self._set_instance( window, None )
+  def deactivate( self ):
+    self._get_instance( ).deactivate()
+    self._set_instance( None )
 
-  def update_ui( self, window ):
-    self._get_instance( window ).update_ui()
+  def update_ui( self ):
+    self._get_instance( ).update_ui()
 
